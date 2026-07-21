ab6a481d449074a669d697c4b2103451c87821c7
achuka99 <achukasimon1@gmail.com>
2026-07-21 20:32:17 +0300

    enabled standard path-based routing (removing the need for #

--- lib/admin_web_main.dart
+++ lib/admin_web_main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'firebase_options.dart';
 import 'src/admin_web/admin_web_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AdminWebApp());
 }
